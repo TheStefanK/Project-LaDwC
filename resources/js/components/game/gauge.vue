17d6fb0afa1ca3ddcf79aea7827ae59e3d4daf11
Stefan <Kumpan.Business@gmail.com>
2020-07-12 06:35:38 +0200

    Update gauge.vue

--- resources/js/components/game/gauge.vue
+++ resources/js/components/game/gauge.vue
@@ -1,17 +1,24 @@
 <template>
-    <div>
-        <img class="test1" src="images/score/bg_fuel2.svg" alt="Demo" width="300px">
-        <img class="test2" src="images/score/zeiger.svg" alt="Demo" height="130px" :style="tankpointer">
-    </div>
+  <div>
+    <img class="test1" src="images/score/bg_fuel2.svg" alt="Demo" width="300px" />
+    <img class="test2" src="images/score/zeiger.svg" alt="Demo" height="130px" :style="tankpointer" />
+    <p></p>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "gauge",
-    props:['tankpointer']
-  }
+export default {
+  name: "gauge",
+  props: ["tankpointer"],
+  methods: {
+    name() {
+      for(i,i++,i<5){
+
+      };
+    }
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
